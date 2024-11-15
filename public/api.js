ad723a90bd6b6563f7c551868f5920e269fb313b
juan963303 <estudiantejuandiego963303944>
2024-11-15 13:10:47 +0100

    update

---
+++ public/api.js
@@ -0,0 +1,12 @@
+const url = "https://api.themoviedb.org/3/trending/movie/day?language=en-US&api_key=a21c9aa641d6ade1164e20c882951889"
+
+
+export const getData = async () =>{
+    const response = await fetch(url);
+    
+    const data = await response.json();
+    // console.log(data)
+    return data;
+}
+
+// getData();
